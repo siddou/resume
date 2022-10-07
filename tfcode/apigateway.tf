358f8919ca80aa3a1a481ee74624d9b95b778a32
Siddou <siddou@siddou.com>
2022-10-07 12:38:06 +0200

    counterapi

--- tfcode/apigateway.tf
+++ tfcode/apigateway.tf
@@ -2,11 +2,11 @@ module "apigateway-v2" {
   source  = "terraform-aws-modules/apigateway-v2/aws"
   version = "~> 2.0.0"
 
-  name = "counter-http-${random_pet.this.id}"
-  # domain_name                 = "counter.${var.AWS_DOMAIN_NAME}"
-  # domain_name_certificate_arn = module.acm-apigateway.acm_certificate_arn
-  description   = "Counter API"
-  protocol_type = "HTTP"
+  name                        = "counter-http-${random_pet.this.id}"
+  domain_name                 = "counter.${var.AWS_DOMAIN_NAME}"
+  domain_name_certificate_arn = module.acm-apigateway.acm_certificate_arn
+  description                 = "Counter API"
+  protocol_type               = "HTTP"
 
   # body = templatefile("./src/swagger.yml", {
   #   function_arn = module.lambda.lambda_function_arn
