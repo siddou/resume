68ca88b029b135a4b839382346df6508afd9ac04
Siddou <siddou@siddou.com>
2022-10-11 19:27:50 +0200

    dynamodb sse

--- tfcode/dynamodb.tf
+++ tfcode/dynamodb.tf
@@ -2,20 +2,18 @@ module "dynamodb-table" {
   source  = "terraform-aws-modules/dynamodb-table/aws"
   version = "~> 2.0.0"
 
-  name                = "cloud-resume-stats"
-  billing_mode        = "PROVISIONED"
-  read_capacity       = 1
-  write_capacity      = 1
-  autoscaling_enabled = false
-  hash_key            = "stat"
+  name                           = "cloud-resume-stats"
+  billing_mode                   = "PROVISIONED"
+  read_capacity                  = 1
+  write_capacity                 = 1
+  autoscaling_enabled            = false
+  hash_key                       = "stat"
+  server_side_encryption_enabled = true
   attributes = [
     {
       name = "stat"
       type = "S"
     },
   ]
-  server_side_encryption {
-    enabled = true
-  }
 }
 
