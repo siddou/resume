0bc46c8662e3863b64c31b1d51cedf94b59bdf92
Siddou <Siddou@siddou.com>
2023-01-17 16:39:53 +0100

    jekyll

---
+++ mkdocs/docs/javascripts/extra.js
@@ -0,0 +1,12 @@
+const counter = document.getElementById("counter");
+
+const updateCounter = async () => {
+    const data = await fetch(
+        "https://counter.malbertini.ovh/increment"
+    );
+    const count = await data.json();
+    counter.innerHTML = count.value;
+    counter.style.filter = "blur(0)";
+};
+
+updateCounter();
